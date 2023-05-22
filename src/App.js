39a1769c887a0778547bc61443509010434fa306
OleksandrB93 <barabashsan4ik@gmail.com>
2023-05-22 21:05:17 +0300

    auth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { withAuthenticator } from "@aws-amplify/ui-react";
 
-function App() {
+function App({ signOut, user }) {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="bg-red-300 text-black mb-4 px-4 py-1 rounded-lg underline">
+          {user.attributes.email}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button className="bg-red-500 px-4 rounded-lg" onClick={signOut}>
+          Sign Out
+        </button>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App, {
+  // socialProviders: ["google"], 
+});
